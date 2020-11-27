549e6ecc669577f0ead18bfd52482d7e7b391635
AndrewBeznosko <andrew.beznosko@gmail.com>
2020-11-27 12:33:13 +0200

    .

--- src/api/cryptocompare.js
+++ src/api/cryptocompare.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const client = axios.create({
     baseURL: process.env.VUE_APP_CRYPTOCOMPARE_API_BASE_URL,
     params: {
-        api_key: process.env.VUE_APP_CRYPTOCOMPARE_API_KEY
+        api_key: process.env.VUE_APP_CRYPTOCOMPARE_API_KEY,
+        extraParams: 'my_crypto'
     }
 });
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,27 @@
   <div class="container vh-100 d-flex flex-column align-items-center justify-content-center">
     <HelloWorld msg="Test Task Crypto"/>
     <div class="row w-100">
-      <div class="col-md-5 col-lg-4">
+      <div v-if="currencies.DISPLAY" class="col-md-5 col-lg-4">
         <h4 class="d-flex justify-content-between align-items-center mb-3">
           <span class="text-muted">Currencies</span>
-          <span class="badge bg-secondary rounded-pill">{{ currencies.length }}</span>
+          <span class="badge bg-secondary rounded-pill">{{ Object.keys(currencies.DISPLAY).length }}</span>
         </h4>
         <ul class="list-group mb-3">
-          <li class="list-group-item d-flex justify-content-between lh-sm">
+          <li v-for="(currency, i) in currencies.DISPLAY" :key="i" class="list-group-item d-flex justify-content-between lh-sm">
             <div>
-              <h6 class="my-0">Product name</h6>
-              <small class="text-muted">Brief description</small>
+              <h6 class="my-0 d-flex align-items-center">
+                <img :src="`${crypto_app_url}/${currency.USD.IMAGEURL}`" alt="" style="max-width: 30px">
+                <span class="ml-3">{{ i }}</span>
+              </h6>
+              <!-- <small class="text-muted">Brief description</small> -->
             </div>
-            <span class="text-muted">$12</span>
+            <div class="d-flex flex-column">
+              <span v-for="(sub_currency, j) in currency" :key="`${i}-${j}`" class="text-muted">
+                {{ sub_currency.PRICE }} 
+              </span>
+            </div>
           </li>
-          <li class="list-group-item d-flex justify-content-between lh-sm">
+          <!-- <li class="list-group-item d-flex justify-content-between lh-sm">
             <div>
               <h6 class="my-0">Second product</h6>
               <small class="text-muted">Brief description</small>
@@ -39,7 +46,7 @@
           <li class="list-group-item d-flex justify-content-between">
             <span>Total (USD)</span>
             <strong>$20</strong>
-          </li>
+          </li> -->
         </ul>
       </div>
     </div>
@@ -58,27 +65,30 @@ export default {
   },
   data() {
     return {
-      currencies: []
+      crypto_app_url: process.env.VUE_APP_CRYPTOCOMPARE_URL,
+      currencies: {},
+      currenciesName: []
     }
   },
   methods: {
     getCurrencies() {
       ApiCryptoCompare.pricemultifull({
         params: {
-          fsyms: 'BTC',
-          tsyms: 'USD,EUR'
+          fsyms: 'BTC,ETH,DASH,REP',
+          tsyms: 'USD'
         }
       })
       .then((res) => {
+        this.currenciesName = Object.getOwnPropertyNames(res.data.DISPLAY)
         this.currencies = res.data
       })
       .catch((err) => {
         console.log(err)
       })
-    }
+    },
   },
   mounted() {
-    console.log(process.env.VUE_APP_CRYPTOCOMPARE_URL)
+    // console.log(process.env.VUE_APP_CRYPTOCOMPARE_URL)
     this.getCurrencies()
   }
 }
